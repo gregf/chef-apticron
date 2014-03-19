6cd0a57fe90574e2a8e6a297f8f622456b502215
Greg Fitzgerald <greg@gregf.org>
2014-03-18 21:30:48 -0400

    Fix Rubocop warnings

--- files/default/test/default_test.rb
+++ files/default/test/default_test.rb
@@ -5,15 +5,15 @@ require 'minitest/spec'
 
 describe_recipe 'apticron::default' do
   describe 'ensures apticron is installed' do
-    let(:binary) { file("/usr/sbin/apticron") }
-    it { binary.must_have(:mode, "0755") }
-    it { binary.must_have(:owner, "root") }
+    let(:binary) { file('/usr/sbin/apticron') }
+    it { binary.must_have(:mode, '0755') }
+    it { binary.must_have(:owner, 'root') }
   end
 
   describe 'ensures apticron.conf config is present' do
     let(:config) { file(node['apticron']['config_file']) }
-    it { config.must_have(:mode, "0644") }
-    it { config.must_have(:owner, "root") }
+    it { config.must_have(:mode, '0644') }
+    it { config.must_have(:owner, 'root') }
     it { config.must_include "EMAIL=#{node['apticron']['email']}" }
   end
 end
